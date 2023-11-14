8a11af7fbc116cd58455395a07dbd68f72e21b3e
Bluchatoe <bluechatoh02@gmail.com>
2023-11-14 11:30:51 +0800

    Checkbox Style
    Changed the checkbox styling pa check winstonks

--- login-part.php
+++ login-part.php
@@ -7,12 +7,13 @@
         <input type="text" name="username" id="username" placeholder="Username">
         <input type="password" name="password" id="password" placeholder="Password">
 
-        <div class="remember-me">
-            <input type="checkbox" name="rememberMe" id="rememberMe">
-            <label for="rememberMe">Remember Me</label>
-        </div>
+        <label class="container">Remember me
+            <input type="checkbox" >
+            <span class="checkmark"></span>
+        </label>
         
         <input type="submit" value="Login" class="login-btn">
+        
     </form>
 
     <button class="forgot-pass-btn">forgot password?</button>
